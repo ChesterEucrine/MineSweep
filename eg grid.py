c1093f8e74eb6445940c7cebab27c0a9f886c1c8
ChesterEucrine <blessingdlamini24747@gmail.com>
2021-03-17 16:22:11 +0200

    Half Baked Sweeper

--- eg grid.py
+++
@@ -1,7 +0,0 @@
-def minesweeper(n):
-    arr = [[0 for row in range(n)] for column in range(n)]
-    for row in arr:
-        print(" ".join(str(cell) for cell in row))
-        print("")
-if __name__ == "__main__":
-    minesweeper(5)
